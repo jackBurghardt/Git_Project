e04ac3a822cc2c8169d0562ca9bcecc84b7ccd5e
Anika Iyer <anika.sule.iyer@gmail.com>
2022-09-18 15:42:18 -0700

    Tree Test File Name
    Created JUnit tester for Tree, added in a test for writing the file and checking the name.

--- src/TreeTester.java
+++ src/TreeTester.java
@@ -1,31 +1,40 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class TreeTester {
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class TreeTester {
 	
-	public static void main(String[] args) throws NoSuchAlgorithmException, IOException
-	{
-		ArrayList<String> list = new ArrayList<String>();
-		list.add("62X : d99ed7744a09c6078ef391a07eca50cd2d3622da");
-		list.add("Anika : 01670a0aee5d4289cb1c8231ac7cd6db7b4e4be8");
-		list.add("Audrey : 9f56d9b292b55162ca3999b56f054b407a2ae67e");
-		list.add("Eric : 6d0e5951f2a9d928c1d17b25d57f0461296048e6");
-		list.add("Karen : 961c4ce5692d8f0f612b4cd6d8665865aac3ace4");
-		list.add("Kensuke : 2fa6905f03507615399918510f07657b129890fb");
-		Tree tree = new Tree(list);
+	static ArrayList<String> robotics = new ArrayList<String>();
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
 		
-		
+		robotics.add("Anika : 01670a0aee5d4289cb1c8231ac7cd6db7b4e4be8");
+		robotics.add("Audrey : 9f56d9b292b55162ca3999b56f054b407a2ae67e");
+		robotics.add("Eric : 6d0e5951f2a9d928c1d17b25d57f0461296048e6");
+		robotics.add("Karen : 961c4ce5692d8f0f612b4cd6d8665865aac3ace4");
+		robotics.add("Kensuke : 2fa6905f03507615399918510f07657b129890fb");
 	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() throws IOException {
+		// Creates a tree which should write out the file
+		Tree tree = new Tree(robotics);
+		// The combined SHA1 should be f692ce3bc466086b97b348d87bf39cbd0a59d06a
+		String SHA1 = "f692ce3bc466086b97b348d87bf39cbd0a59d06a";
+		File file = new File(SHA1);
+		// Checks if the file with the right SHA1 name exists
+		assertTrue(file.exists());
+	}
+
 }
