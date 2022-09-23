6278e79e537698872c5974facf53e2437eeec1fe
JackBurghardt <jackburghardt@yahoo.com>
2022-09-22 19:58:37 -0700

    Changed tree class

--- src/Tree.java
+++ src/Tree.java
@@ -10,6 +10,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
+
 public class Tree {
 	private String contents = ""; 
 	private String hashedContents;
